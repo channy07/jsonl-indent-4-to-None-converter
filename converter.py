a1bde2965ee46327d780f2e34cd0da24fef954ab
channy7123 <76421850+channy07@users.noreply.github.com>
2024-12-16 13:37:56 +0900

    Update converter.py

--- converter.py
+++ converter.py
@@ -1,10 +1,9 @@
 import json
 
 # 입력 파일과 출력 파일 경로 설정
-input_file = input("filepath : ")  # 원본 JSONL 파일 경로
-output_file = 'output.jsonl'  # 변경된 JSONL 파일 경로
+input_file = input("filepath : ") 
+output_file = 'output.jsonl'  
 
-# JSONL 파일을 읽고, 재포맷하여 출력 파일에 쓰기
 with open(input_file, 'r', encoding='utf-8') as infile, open(output_file, 'w', encoding='utf-8') as outfile:
 
     lines = ""
@@ -12,7 +11,7 @@ with open(input_file, 'r', encoding='utf-8') as infile, open(output_file, 'w', e
     for line in infile:
         if(line == "}\n") :
             lines += line.strip()
-            outfile.write(lines + '\n')  # 한 줄로 작성하여 출력
+            outfile.write(lines + '\n') 
             lines = ""
 
         lines += line.strip()
